0cfdb5cc051cb3d161da0fd2b8eef98d339822f7
Alistair Buxton <a.j.buxton@gmail.com>
2018-05-28 15:26:10 +0100

    Move the dkms_src tarball to dkms package dir.

--- doit.sh
+++ doit.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-cd media_build && ./build_all.sh ../linux_media/ -dt
+cd media_build && \
+    ./build_all.sh ../linux_media/ -dt && \
+    ./create_dkms_tar.sh && \
+    mv media-build-*.dkms_src.tgz ../media-build-dkms
+
 
 
