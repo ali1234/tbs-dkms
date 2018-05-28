78abbbcaa3d63749222fa8780eea28ed2c9f24ab
Alistair Buxton <a.j.buxton@gmail.com>
2018-05-28 15:28:02 +0100

    Build the deb package

--- doit.sh
+++ doit.sh
@@ -3,7 +3,12 @@
 cd media_build && \
     ./build_all.sh ../linux_media/ -dt && \
     ./create_dkms_tar.sh && \
-    mv media-build-*.dkms_src.tgz ../media-build-dkms
+    mv media-build-*.dkms_src.tgz ../media-build-dkms && \
+    cd ..
 
 
-
+cd media-build-dkms && \
+    ./prepare_dkms_src.sh ubuntu bionic && \
+   fakeroot debian/rules binary && \
+   debuild -S && \
+   debuild -i -us -uc -b
