7e63259ca712a698983cf3d555355ff861543d71
Alistair Buxton <a.j.buxton@gmail.com>
2018-05-28 15:32:56 +0100

    Use sh -e for easier error checking.

--- doit.sh
+++ doit.sh
@@ -1,14 +1,27 @@
-#!/bin/sh
+#!/bin/sh -e
 
-cd media_build && \
-    ./build_all.sh ../linux_media/ -dt && \
-    ./create_dkms_tar.sh && \
-    mv media-build-*.dkms_src.tgz ../media-build-dkms && \
-    cd ..
+cd media_build
 
+# prepare linux_media tarball
+./build_all.sh ../linux_media/ -dt
 
-cd media-build-dkms && \
-    ./prepare_dkms_src.sh ubuntu bionic && \
-   fakeroot debian/rules binary && \
-   debuild -S && \
-   debuild -i -us -uc -b
+# prepare media_build tarball for dkms
+# includes linux_media tarball
+./create_dkms_tar.sh
+
+# move media_build tarball to dkms builder
+mv media-build-*.dkms_src.tgz ../media-build-dkms
+
+cd ../media-build-dkms
+
+# extract media_build tarball
+./prepare_dkms_src.sh ubuntu bionic
+
+# create a binary package
+fakeroot debian/rules binary
+
+# create the source package and sign it
+debuild -S
+
+# create the binary package (no signing)
+debuild -i -us -uc -b
